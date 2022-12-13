b8c32d20ae6736af2e73b8a57b62942466841ad3
Rifqi Favian <piyanh90@gmail.com>
2022-12-13 20:10:36 +0700

    latihan baru

--- game suwit jawa/game suwit jawa.js
+++ game suwit jawa/game suwit jawa.js
@@ -1,35 +1,48 @@
-let randomA = Math.floor(Math.random() * 3);
+function pilihanComputer() {
+  let randomA = Math.floor(Math.random() * 3);
 
-let comp = "";
-if ((randomA = 0)) {
-  comp = "kelingking";
-} else if ((randomA = 1)) {
-  comp = "jempol";
-} else {
-  comp = "telunjuk";
+  if (randomA == 0) return "kelingking";
+  if (randomA == 1) return "jempol";
+  if (randomA == 2) return "telunjuk";
 }
 
-konfirmasi = true;
-while (konfirmasi === true) {
-  let inputan = prompt("masukkan jari yang kamu pilih (telunjuk, jempol, kelingking) :");
-
-  if (inputan == comp) {
-    alert("Hasilnya seri!");
-  } else if (inputan == "kelingking") {
-    comp == "jempol"
-      ? alert("Komputer memilih : " + comp + " dan kamu memilih : " + inputan + " maka kamu menang!")
-      : alert("Komputer memilih : + " + comp + " dan kamu memilih : " + inputan + " maka kamu kalah!");
-  } else if (inputan == "jempol") {
-    comp == "telunjuk"
-      ? alert("Komputer memilih : " + comp + " dan kamu memilih : " + inputan + " maka kamu menang!")
-      : alert("Komputer memilih : " + comp + " dan kamu memilih : " + inputan + " maka kamu kalah!");
-  } else if (inputan == "telunjuk") {
-    comp == "kelingking"
-      ? alert("Komputer memilih : " + comp + " dan kamu memilih : " + inputan + " maka kamu menang!")
-      : alert("Komputer memilih : " + comp + " dan kamu memilih : " + inputan + " maka kamu kalah!");
-  } else {
-    alert("Prompt yang kamu masukkan salah!");
-  }
-
-  konfirmasi = confirm("Ingin bermain lagi?");
+function getHasil(inputan, comp) {
+  if (inputan == comp) return "Seri";
+  if (inputan == "kelingking") return comp == "jempol" ? "Menang" : "Kalah";
+  if (inputan == "jempol") return comp == "telunjuk" ? "Menang" : "Kalah";
+  if (inputan == "telunjuk") return comp == "kelingking" ? "Menang" : "Kalah";
 }
+
+function putar() {
+  imgKomputer = document.querySelector(".img-komputer");
+  const gambar = ["kelingking", "telunjuk", "jempol"];
+  let i = 0;
+  waktuAwal = new Date().getTime();
+  setInterval(function () {
+    if (new Date().getTime() - waktuAwal > 1000) {
+      clearInterval;
+      return;
+    }
+    imgKomputer.setAttribute("src", "img/" + gambar[i++] + ".png");
+    if (i == gambar.length) {
+      i = 0;
+    }
+  }, 100);
+}
+gambarPlayer = document.querySelectorAll("ul li img");
+gambarPlayer.forEach((gambar) => {
+  gambar.addEventListener("click", function () {
+    pilihanPlayer = gambar.className;
+    pilihanCom = pilihanComputer();
+    hasil = getHasil(pilihanPlayer, pilihanCom);
+    console.log(pilihanCom);
+    putar();
+    setTimeout(function () {
+      imgKomputer = document.querySelector(".img-komputer");
+      imgKomputer.setAttribute("src", "img/" + pilihanCom + ".png");
+
+      infoHasil = document.querySelector(".info");
+      infoHasil.innerHTML = hasil;
+    }, 1000);
+  });
+});
